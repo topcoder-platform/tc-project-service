2911f6922259fdf21f00cb07b07a889933c78ea8
Parth Shah <parth@topcoder.com>
2016-10-10 11:05:31 -0700

    making direct project svc timeout configurable

--- src/services/directProject.js
+++ src/services/directProject.js
@@ -16,7 +16,7 @@ function _getHttpClient(req){
     httpClient.defaults.headers.common['Authorization'] = req.headers.authorization
     httpClient.defaults.headers.common['Content-Type'] = 'application/json'
     httpClient.defaults.baseURL = config.get('directProjectServiceEndpoint')
-    httpClient.defaults.timeout = 4000
+    httpClient.defaults.timeout = _.get(config, 'directProjectServiceTimeout', 5000)
     httpClient.interceptors.response.use((resp) => {
         // req.log.debug('resp: ', JSON.stringify(resp.data, null, 2))
         if (resp.status !== 200 || resp.data.result.status !== 200) {
