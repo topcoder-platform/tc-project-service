e863b263df61ed9a3e3c6085b343cfce5a1a0bfe
Parth Shah <parth@topcoder.com>
2016-11-17 07:47:59 -0800

    updating bean stalk config and deploy script

--- ebs_deploy.sh
+++ ebs_deploy.sh
@@ -14,9 +14,9 @@ export AWS_SECRET_ACCESS_KEY=$(eval "echo \$${ENV}_AWS_SECRET_ACCESS_KEY")
 # eb deploy
 eb init -r us-east-1 $SERVICE
 EB_OUTPUT="$(eb deploy -l $TAG -r us-east-1)"
+
 echo $EB_OUTPUT
-if [[ $EB_OUTPUT =~ .*ERROR.* ]]
-then
+if echo $EB_OUTPUT | grep -iq error; then
  exit 1
 fi
 exit 0
