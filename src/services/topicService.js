007c75183bbf5ec15740f80fac72972d2db52bdf
Parth Shah <parth@topcoder.com>
2016-12-21 13:01:28 -0800

    increasing timeout for topic creation

--- src/services/topicService.js
+++ src/services/topicService.js
@@ -16,7 +16,7 @@ function _getHttpClient(req){
     var httpClient = util.getHttpClient(req)
     httpClient.defaults.headers.common['Authorization'] = req.headers.authorization
     httpClient.defaults.baseURL = config.get('topicServiceEndpoint')
-    httpClient.defaults.timeout = 3000
+    httpClient.defaults.timeout = 30000
     httpClient.interceptors.response.use(resp => {
         // req.log.debug('resp: ', JSON.stringify(resp.data, null, 2))
         if (resp.status !== 200 || resp.data.result.status !== 200) {
