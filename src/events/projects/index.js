d4a6eede1aa5d2b821ab12d74526fabd33cf77a4
Parth Shah <parth@topcoder.com>
2017-02-01 10:28:35 -0800

    fixes #35

--- src/events/projects/index.js
+++ src/events/projects/index.js
@@ -5,32 +5,6 @@ import {
 import util from '../../util'
 import config from 'config'
 import querystring from 'querystring'
-import topicService from '../../services/topicService'
-
-const _addProjectStatus = (req, logger, project) => {
-  const topics = [
-    {
-      title: 'Hello, Coder here! Your project has been created successfully',
-      body: 'It took almost 245ms for me to create it, but all is good now.\
-        That\'s a lot of hard work for a robot, you know!'
-    }, {
-      title: 'Hey there, I\'m ready with the next steps for your project!',
-      body: `<p>I went over the project and I see we still need to collect more\
-       details before I can use my super computational powers and create your quote.</p>\
-       <p>Head over to the <a href="/projects/${project.id}/specification/">Specification</a>\
-        section and answer all of the required questions. If you already have a document\
-        with specification, verify it against our checklist and upload it.</p>`
-    }
-  ]
-  // NOTE: running these in sequence cos we want topic[0] to be created first
-  // firing these events in parallel doesn't ensure that topic[0] is created first
-  return topicService.createTopic(req, project.id, topics[0].title, topics[0].body)
-    .then(() => topicService.createTopic(req, project.id, topics[1].title, topics[1].body))
-    .then(() => {
-      logger.debug('post-project creation messages posted')
-      return true
-    })
-}
 
 /**
  * Creates a lead in salesforce for the connect project.
@@ -84,7 +58,6 @@ const projectCreatedHandler = (logger, msg, channel) => {
         }
       }
       return Promise.all([
-        _addProjectStatus(req, logger, project),
         _addSalesforceLead(token, logger, project).then((resp)=> logger.debug('web to lead response:', resp.status))
       ]);
     })
