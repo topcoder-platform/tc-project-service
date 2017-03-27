abd486b652fcc418574204446bfedb786d912762
Parth Shah <parth@topcoder.com>
2017-03-27 14:48:29 -0700

    fixing analytics and project history

--- src/routes/projects/create.js
+++ src/routes/projects/create.js
@@ -111,6 +111,13 @@ module.exports = [
             if (newProject.billingAccountId) {
               body.billingAccountId = newProject.billingAccountId;
             }
+            // add to project history
+            models.ProjectHistory.create({
+              projectId: _newProject.id,
+              status: PROJECT_STATUS.DRAFT,
+              cancelReason: null,
+              updatedBy: req.authUser.userId,
+            });
             return directProject.createDirectProject(req, body)
               .then((resp) => {
                 newProject.directProjectId = resp.data.result.content.projectId;
@@ -124,6 +131,7 @@ module.exports = [
                 return Promise.resolve();
               });
           })
+
           .then(() => {
             newProject = newProject.get({ plain: true });
             // remove utm details & deletedAt field
