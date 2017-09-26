8d1d428f4f8e184532c365632384499f1fc2fa5d
Vikas Agarwal <vikas.agarwal@topcoder.com>
2017-09-26 13:12:26 +0530

    Fixed lint errors

--- src/constants.js
+++ src/constants.js
@@ -4,10 +4,10 @@ export const PROJECT_TYPE = {
   GENERIC: 'generic',
   VISUAL_PROTOTYPE: 'visual_prototype',
   VISUAL_DESIGN: 'visual_design',
-  WEBSITE : 'website',
+  WEBSITE: 'website',
   APP: 'app',
   QUALITY_ASSURANCE: 'quality_assurance',
-  CHATBOT: 'chatbot'
+  CHATBOT: 'chatbot',
 };
 
 export const PROJECT_STATUS = {
