bc973e085d9cad1daa9638c606e85ec80157c160
Samir Gondzetovic <gondzo@gmail.com>
2018-01-28 12:13:50 +0000

    add attachment download endpoint

--- src/routes/attachments/download.js
+++ src/routes/attachments/download.js
@@ -17,24 +17,30 @@ module.exports = [
     const projectId = _.parseInt(req.params.projectId);
     const attachmentId = _.parseInt(req.params.id);
 
-    return models.ProjectAttachment.findOne(
-    	{
-			where: {
-				id: attachmentId,
-				projectId,
-			},
-		})
-        .then((attachment) => {
-          if (!attachment) {
-          	next({status:404});
-          	return;
-          }
-          return util.getFileDownloadUrl(req, attachment.filePath)
-        })
-        .then((result) => {
-          const url = result[1];
-          console.log(url);
-          res.status(200).json(util.wrapResponse(req.id, {url}));
-        });    
+    models.ProjectAttachment.findOne(
+      {
+        where: {
+          id: attachmentId,
+          projectId,
+        },
+      })
+    .then((attachment) => {
+      if (!attachment) {
+        const err = new Error('Record not found');
+        err.status = 404;
+        return Promise.reject(err);
+      }
+      return util.getFileDownloadUrl(req, attachment.filePath);
+    })
+    .then((result) => {
+      const url = result[1];
+      res.status(200).json(util.wrapResponse(req.id, { url }));
+    })
+    .catch((error) => {
+      req.log.error('Error fetching attachment', error);
+      const rerr = error;
+      rerr.status = rerr.status || 500;
+      next(rerr);
+    });
   },
 ];
