1c368e89147f31ee54c17ca33cd97dc1dbf192e4
Vikas Agarwal <vikas.agarwal@topcoder.com>
2018-05-08 12:26:22 +0530

    Fixing region env variable access

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@
 JQ="jq --raw-output --exit-status"
 
 ENV=$1
+AWS_REGION=$(eval "echo \$${ENV}_AWS_REGION")
 ACCOUNT_ID=$(eval "echo \$${ENV}_AWS_ACCOUNT_ID")
 AWS_REPOSITORY=$(eval "echo \$${ENV}_AWS_REPOSITORY")
 
