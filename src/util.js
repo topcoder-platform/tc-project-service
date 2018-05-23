0a319e797b13cd61a9e3e4936da91b5e9dfed3ca
Sachin Maheshwari <sachin.maheshwari@topcoder.com>
2018-05-23 13:55:56 +0530

    debugging done.

--- src/util.js
+++ src/util.js
@@ -114,9 +114,9 @@ _.assignIn(util, {
    */
   hasAdminRole: (req) => {
     const isMachineToken = _.get(req, 'authUser.isMachine', false);
-    // const tokenScopes = _.get(req, 'authUser.scopes', []);
+    const tokenScopes = _.get(req, 'authUser.scopes', []);
     if (isMachineToken) {
-      // if (_.indexOf(tokenScopes, TOKEN_SCOPES.CONNECT_PROJECT_ADMIN) >= 0) return true;
+      if (_.indexOf(tokenScopes, TOKEN_SCOPES.CONNECT_PROJECT_ADMIN) >= 0) return true;
       return true;
     }
     let roles = _.get(req, 'authUser.roles', []);
