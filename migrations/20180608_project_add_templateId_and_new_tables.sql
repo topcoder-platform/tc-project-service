56cafd18165fcf091242917088b8e515af2b9cc1
ngoctay <phamminhngoc@gmail.com>
2018-06-10 13:43:16 +0700

    Merge migration scripts

--- migrations/20180608_project_add_templateId_and_new_tables.sql
+++ migrations/20180608_project_add_templateId_and_new_tables.sql
@@ -2,6 +2,7 @@
 -- UPDATE EXISTING TABLES:
 --   projects
 --     templateId column: added
+--     version column: added
 -- CREATE NEW TABLES:
 --   milestones
 --   phase_products
@@ -17,6 +18,11 @@
 -- projects
 --
 ALTER TABLE projects ADD COLUMN "templateId" bigint;
+
+-- make sure to update existing projects to have this field set to "v2"
+ALTER TABLE projects ADD COLUMN "version" varchar(3) NOT NULL DEFAULT 'v2';
+-- make sure new projects from now on have "v3" as default value
+ALTER TABLE projects ALTER COLUMN "version" SET DEFAULT 'v3';
 
 --
 -- milestones
