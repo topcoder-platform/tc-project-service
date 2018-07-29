127a47024fd863b6e70130ccc307112c2eb83f0b
mhikeo <mhikeo@gmail.com>
2018-07-29 12:13:48 +0800

    Fix for Issue #129

---
+++ src/routes/milestoneTemplates/clone.js
@@ -0,0 +1,91 @@
+/**
+ * API to clone a milestone template
+ */
+import validate from 'express-validation';
+import _ from 'lodash';
+import Joi from 'joi';
+import Sequelize from 'sequelize';
+import { middleware as tcMiddleware } from 'tc-core-library-js';
+import util from '../../util';
+import models from '../../models';
+
+const permissions = tcMiddleware.permissions;
+
+const schema = {
+  params: {
+    productTemplateId: Joi.number().integer().positive().required(),
+  },
+  body: {
+    param: Joi.object().keys({
+      sourceTemplateId: Joi.number().integer().positive().required(),
+    }).required(),
+  },
+};
+
+module.exports = [
+  validate(schema),
+  permissions('milestoneTemplate.clone'),
+  (req, res, next) => {
+    let result;
+
+    return models.sequelize.transaction(tx =>
+      // Find the product template
+      models.ProductTemplate.findAll({ where: { id: [req.params.productTemplateId, req.body.param.sourceTemplateId] }, transaction: tx })
+        .then((productTemplates) => {
+          // Not found
+          if (!productTemplates) {
+            const apiErr = new Error(
+              `Product template not found for product template ids ${req.params.productTemplateId} ${req.body.param.sourceTemplateId}`);
+            apiErr.status = 404;
+            return Promise.reject(apiErr);
+          }
+
+          const targetProductTemplate = _.find(productTemplates, template => { return template.id == req.params.productTemplateId });
+          const sourceProductTemplate = _.find(productTemplates, template => { return template.id == req.body.param.sourceTemplateId });
+
+          // Not found
+          if (!targetProductTemplate) {
+            const apiErr = new Error(
+              `Product template not found for product template id ${req.params.productTemplateId}`);
+            apiErr.status = 404;
+            return Promise.reject(apiErr);
+          }
+
+          // Not found
+          if (!sourceProductTemplate) {
+            const apiErr = new Error(
+              `Product template not found for source product template id ${req.body.param.sourceTemplateId}`);
+            apiErr.status = 404;
+            return Promise.reject(apiErr);
+          }
+
+          return models.ProductMilestoneTemplate.findAll({
+            where: {
+              productTemplateId: req.body.param.sourceTemplateId,
+            },
+            attributes: { exclude: ['id', 'deletedAt', 'createdAt', 'updatedAt', 'deletedBy'] },
+            raw: true,
+          })
+          .then((milestoneTemplatesToClone) => {
+            let newMilestoneTemplates = _.cloneDeep(milestoneTemplatesToClone)
+            _.each(newMilestoneTemplates, milestone => {
+              milestone.productTemplateId = req.params.productTemplateId
+              milestone.createdBy = req.authUser.userId
+              milestone.updatedBy = req.authUser.userId
+            })
+            return models.ProductMilestoneTemplate.bulkCreate(newMilestoneTemplates, { transaction: tx });
+          })
+        })
+        .then((createdEntities) => {
+          // Omit deletedAt and deletedBy
+          result = _.map(createdEntities, entity => _.omit(entity, 'deletedAt', 'deletedBy'))
+          return result
+        }),
+    )
+    .then(() => {
+      // Write to response
+      res.status(201).json(util.wrapResponse(req.id, result, 1, 201));
+    })
+    .catch(next);
+  },
+];
---
+++ src/routes/milestoneTemplates/clone.spec.js
@@ -0,0 +1,234 @@
+/**
+ * Tests for create.js
+ */
+import chai from 'chai';
+import request from 'supertest';
+import _ from 'lodash';
+import server from '../../app';
+import testUtil from '../../tests/util';
+import models from '../../models';
+
+const should = chai.should();
+
+const productTemplates = [
+  {
+    name: 'name 1',
+    productKey: 'productKey 1',
+    category: 'category',
+    icon: 'http://example.com/icon1.ico',
+    brief: 'brief 1',
+    details: 'details 1',
+    aliases: {
+      alias1: {
+        subAlias1A: 1,
+        subAlias1B: 2,
+      },
+      alias2: [1, 2, 3],
+    },
+    template: {
+      template1: {
+        name: 'template 1',
+        details: {
+          anyDetails: 'any details 1',
+        },
+        others: ['others 11', 'others 12'],
+      },
+      template2: {
+        name: 'template 2',
+        details: {
+          anyDetails: 'any details 2',
+        },
+        others: ['others 21', 'others 22'],
+      },
+    },
+    createdBy: 1,
+    updatedBy: 2,
+  },
+  {
+    name: 'template 2',
+    productKey: 'productKey 2',
+    category: 'category',
+    icon: 'http://example.com/icon2.ico',
+    brief: 'brief 2',
+    details: 'details 2',
+    aliases: {},
+    template: {},
+    createdBy: 3,
+    updatedBy: 4,
+    deletedAt: new Date(),
+  },
+];
+const milestoneTemplates = [
+  {
+    name: 'milestoneTemplate 1',
+    duration: 3,
+    type: 'type1',
+    order: 1,
+    productTemplateId: 1,
+    plannedText: 'text to be shown in planned stage',
+    blockedText: 'text to be shown in blocked stage',
+    activeText: 'text to be shown in active stage',
+    completedText: 'text to be shown in completed stage',
+    createdBy: 1,
+    updatedBy: 2,
+  },
+  {
+    name: 'milestoneTemplate 2',
+    duration: 4,
+    type: 'type2',
+    order: 2,
+    plannedText: 'text to be shown in planned stage - 2',
+    blockedText: 'text to be shown in blocked stage - 2',
+    activeText: 'text to be shown in active stage - 2',
+    completedText: 'text to be shown in completed stage - 2',
+    productTemplateId: 1,
+    createdBy: 2,
+    updatedBy: 3,
+  },
+];
+
+describe('CLONE milestone template', () => {
+  beforeEach(() => testUtil.clearDb()
+    .then(() => models.ProductTemplate.bulkCreate(productTemplates))
+    .then(() => models.ProductMilestoneTemplate.bulkCreate(milestoneTemplates)),
+  );
+  after(testUtil.clearDb);
+
+  describe('POST /productTemplates/{productTemplateId}/milestones/clone', () => {
+    const body = {
+      param: {
+        sourceTemplateId: 1,
+      },
+    };
+
+    it('should return 403 if user is not authenticated/clone', (done) => {
+      request(server)
+        .post('/v4/productTemplates/2/milestones')
+        .send(body)
+        .expect(403, done);
+    });
+
+    it('should return 403 for member', (done) => {
+      request(server)
+        .post('/v4/productTemplates/2/milestones/clone')
+        .set({
+          Authorization: `Bearer ${testUtil.jwts.member}`,
+        })
+        .send(body)
+        .expect(403, done);
+    });
+
+    it('should return 403 for copilot', (done) => {
+      request(server)
+        .post('/v4/productTemplates/2/milestones/clone')
+        .set({
+          Authorization: `Bearer ${testUtil.jwts.copilot}`,
+        })
+        .send(body)
+        .expect(403, done);
+    });
+
+    it('should return 403 for manager', (done) => {
+      request(server)
+        .post('/v4/productTemplates/2/milestones/clone')
+        .set({
+          Authorization: `Bearer ${testUtil.jwts.manager}`,
+        })
+        .send(body)
+        .expect(403, done);
+    });
+
+    it('should return 404 for non-existent product template', (done) => {
+      request(server)
+        .post('/v4/productTemplates/1000/milestones/clone')
+        .set({
+          Authorization: `Bearer ${testUtil.jwts.admin}`,
+        })
+        .send(body)
+        .expect(404, done);
+    });
+
+    it('should return 404 for non-existent source product template', (done) => {
+      const invalidBody = {
+        param: {
+          sourceTemplateId: 99,
+        },
+      };
+
+      request(server)
+        .post('/v4/productTemplates/2/milestones/clone')
+        .set({
+          Authorization: `Bearer ${testUtil.jwts.admin}`,
+        })
+        .send(invalidBody)
+        .expect(404, done);
+    });
+
+    it('should return 422 if missing sourceTemplateId', (done) => {
+      const invalidBody = {
+        param: {
+          sourceTemplateId: undefined,
+        },
+      };
+
+      request(server)
+        .post('/v4/productTemplates/2/milestones/clone')
+        .set({
+          Authorization: `Bearer ${testUtil.jwts.admin}`,
+        })
+        .send(invalidBody)
+        .expect('Content-Type', /json/)
+        .expect(422, done);
+    });
+
+    it('should return 201 for admin', (done) => {
+      request(server)
+        .post('/v4/productTemplates/2/milestones/clone')
+        .set({
+          Authorization: `Bearer ${testUtil.jwts.admin}`,
+        })
+        .send(body)
+        .expect('Content-Type', /json/)
+        .expect(201)
+        .end((err, res) => {
+          const resJson = res.body.result.content;
+          should.exist(resJson.id);
+          resJson.name.should.be.eql(body.param.name);
+          resJson.description.should.be.eql(body.param.description);
+          resJson.duration.should.be.eql(body.param.duration);
+          resJson.type.should.be.eql(body.param.type);
+          resJson.order.should.be.eql(body.param.order);
+          resJson.plannedText.should.be.eql(body.param.plannedText);
+          resJson.blockedText.should.be.eql(body.param.blockedText);
+          resJson.activeText.should.be.eql(body.param.activeText);
+          resJson.completedText.should.be.eql(body.param.completedText);
+
+          resJson.createdBy.should.be.eql(40051333); // admin
+          should.exist(resJson.createdAt);
+          resJson.updatedBy.should.be.eql(40051333); // admin
+          should.exist(resJson.updatedAt);
+          should.not.exist(resJson.deletedBy);
+          should.not.exist(resJson.deletedAt);
+
+          done();
+        });
+    });
+
+    it('should return 201 for connect admin', (done) => {
+      request(server)
+        .post('/v4/productTemplates/2/milestones')
+        .set({
+          Authorization: `Bearer ${testUtil.jwts.connectAdmin}`,
+        })
+        .send(body)
+        .expect('Content-Type', /json/)
+        .expect(201)
+        .end((err, res) => {
+          const resJson = res.body.result.content;
+          resJson.createdBy.should.be.eql(40051336); // connect admin
+          resJson.updatedBy.should.be.eql(40051336); // connect admin
+          done();
+        });
+    });
+  });
+});
