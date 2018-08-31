e11ea6e69aacdf35c620962f2d96ddaacad96cc9
Vikas Agarwal <vikas.agarwal@topcoder.com>
2018-08-31 15:52:55 +0530

    lint fix

--- src/middlewares/performanceRequestLogger.js
+++ src/middlewares/performanceRequestLogger.js
@@ -2,7 +2,7 @@ import coreLib from 'tc-core-library-js';
 
 module.exports = function logRequest(logger) {
   if (!logger) {
-    throw new Error('Logger must be provided')
+    throw new Error('Logger must be provided');
   }
 
   // Use the logger from core lib for non-dev environment
@@ -12,23 +12,24 @@ module.exports = function logRequest(logger) {
 
   // Use the logger with memory usage info
   return (req, res, next) => {
-    var startOpts = {
+    const startOpts = {
       method: req.method,
       url: req.url,
     };
     // Create a per-request child
-    req.log = res.log = logger.child({ requestId: req.id });
+    req.log = logger.child({ requestId: req.id });
+    res.log = req.log;
     req.log.info('start request', startOpts);
     const time = process.hrtime();
-    res.on('finish', function responseSent() {
+    res.on('finish', () => {
       const diff = process.hrtime(time);
       res.log.info('end request', {
         method: startOpts.method,
         url: startOpts.url,
         statusCode: res.statusCode,
         statusMessage: res.statusMessage,
-        duration: diff[0] * 1e3 + diff[1] * 1e-6,
-        heapUsed: process.memoryUsage().heapUsed
+        duration: diff[0] * 1e3 + diff[1] * 1e-6, // eslint-disable-line no-mixed-operators
+        heapUsed: process.memoryUsage().heapUsed,
       });
     });
 
