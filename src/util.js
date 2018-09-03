a1a3be64b63239aba39078210a1821b86cc1d03a
Vikas Agarwal <vikas.agarwal@topcoder.com>
2018-09-03 17:39:01 +0530

    Revert "Revert utils.js#getTopcoderUser()"
    This reverts commit cddec286cabd475f836abdaf2cfbc689678fc52a.

--- src/util.js
+++ src/util.js
@@ -307,14 +307,19 @@ _.assignIn(util, {
     httpClient.defaults.headers.common.Accept = 'application/json';
     httpClient.defaults.headers.common['Content-Type'] = 'application/json';
     httpClient.defaults.headers.common.Authorization = `Bearer ${jwtToken}`;
-    return httpClient.get(`${config.identityServiceEndpoint}users/${userId}`).then((response) => {
-      if (response.data && response.data.result
+    return httpClient.get(`${config.identityServiceEndpoint}users`, {
+      params: {
+        filter: `id=${userId}`,
+      },
+    })
+      .then((response) => {
+        if (response.data && response.data.result
         && response.data.result.status === 200 && response.data.result.content
-        && response.data.result.content.length > 0) {
-        return response.data.result.content[0];
-      }
-      return null;
-    });
+        && response.data.result.content.length === 1) {
+          return response.data.result.content[0];
+        }
+        return null;
+      });
   },
 
   /**
