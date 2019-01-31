faf9578020f9f7e875d9ee7ffa50e3de467c1dee
phoenix303 <divyalife526@gmail.com>
2019-01-31 00:55:06 -0800

    Organization config final fixes

--- src/routes/orgConfig/list.js
+++ src/routes/orgConfig/list.js
@@ -1,17 +1,30 @@
 /**
  * API to list organization config
  */
+import validate from 'express-validation';
+import Joi from 'joi';
 import { middleware as tcMiddleware } from 'tc-core-library-js';
 import models from '../../models';
 import util from '../../util';
 
 const permissions = tcMiddleware.permissions;
 
+const schema = {
+  query: {
+    filter: Joi.string().required(),
+  },
+};
+
 module.exports = [
+  validate(schema),
   permissions('orgConfig.view'),
   (req, res, next) => {
     // handle filters
     const filters = util.parseQueryFilter(req.query.filter);
+    // Throw error if orgId is not present in filter
+    if (!filters.orgId) {
+      return next(util.buildApiError('Missing filter orgId', 422));
+    }
     if (!util.isValidFilter(filters, ['orgId', 'configName'])) {
       return util.handleError('Invalid filters', null, req, next);
     }
