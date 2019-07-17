d2dee07016e44faca4e38192673b57add9223d67
Maksym Mykhailenko <maxceem@gmail.com>
2019-07-17 11:17:05 +0800

    implementation of general methods to check permissions: matchPermissionRule, hasPermission and hasPermissionForProject refactored workManagementForTemplate permission middleware to reuse general method permission method

--- src/permissions/workManagementForTemplate.js
+++ src/permissions/workManagementForTemplate.js
@@ -8,75 +8,44 @@ import models from '../models';
  * @param {String}    policy      the work management permission policy
  * @return {Promise}              Returns a promise
  */
-module.exports = policy => freq => new Promise((resolve, reject) => {
-  const projectId = _.parseInt(freq.params.projectId);
+module.exports = policy => req => new Promise((resolve, reject) => {
+  const projectId = _.parseInt(req.params.projectId);
+
   return models.Project.findOne({
     where: {
       id: projectId,
     },
   })
-        .then((project) => {
-          if (!project) {
-            return resolve(true);
-          }
+    .then((project) => {
+      if (!project) {
+        const apiErr = new Error(`Project not found for id ${projectId}`);
+        apiErr.status = 404;
+        return Promise.reject(apiErr);
+      }
 
-          if (!project.templateId) {
-            const errorMessage = 'You do not have permissions to perform this action';
-            return reject(new Error(errorMessage));
-          }
+      if (!project.templateId) {
+        return null;
+      }
 
-          return models.WorkManagementPermissions.findOne({
-            where: {
-              policy,
-              projectTemplateId: project.templateId,
-            },
-          });
-        })
-        .then((management) => {
-          if (!management) {
-            const errorMessage = 'You do not have permissions to perform this action';
-            return reject(new Error(errorMessage));
-          }
+      return models.WorkManagementPermissions.findOne({
+        where: {
+          policy,
+          projectTemplateId: project.templateId,
+        },
+      });
+    })
+    .then((permission) => {
+      if (!permission) {
+        return false;
+      }
 
-          return models.ProjectMember.getActiveProjectMembers(projectId)
-          .then((members) => {
-            const req = freq;
-            const mem = _.find(members, m => m.userId === req.authUser.userId);
-            let allowRule = false;
-            if (management.allowRule) {
-              if (management.allowRule.projectRoles
-                && management.allowRule.projectRoles.length > 0
-                && !_.isUndefined(mem)) {
-                allowRule = allowRule || _.includes(management.allowRule.projectRoles, mem.role);
-              }
-              if (management.allowRule.topcoderRoles && management.allowRule.topcoderRoles.length > 0) {
-                allowRule = allowRule || util.hasRoles(freq, management.allowRule.topcoderRoles);
-              }
-            }
-            if (management.denyRule) {
-              let denyRuleProject = false;
-              let denyRuleTopcoder = false;
-              if (management.denyRule.projectRoles
-                && management.denyRule.projectRoles.length > 0
-                && !_.isUndefined(mem)) {
-                denyRuleProject = _.includes(management.denyRule.projectRoles, mem.role);
-              }
-              if (management.denyRule.topcoderRoles && management.denyRule.topcoderRoles.length > 0) {
-                denyRuleTopcoder = util.hasRoles(freq, management.denyRule.topcoderRoles);
-              }
-
-              const denyRule = (denyRuleProject || denyRuleTopcoder);
-              return !denyRule && allowRule;
-            }
-
-            return allowRule;
-          });
-        })
-        .then((hasAccess) => {
-          if (!hasAccess) {
-            const errorMessage = 'You do not have permissions to perform this action';
-            return reject(new Error(errorMessage));
-          }
-          return resolve(true);
-        });
+      return util.hasPermissionForProject(permission, req.authUser, projectId);
+    })
+    .then((hasAccess) => {
+      if (!hasAccess) {
+        const errorMessage = 'You do not have permissions to perform this action';
+        return reject(new Error(errorMessage));
+      }
+      return resolve(true);
+    });
 });
