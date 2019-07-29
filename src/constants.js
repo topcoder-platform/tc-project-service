3e85a221860289ba1a89df0f8a2ccc9f33166643
Sachin Maheshwari <sachin.maheshwari@topcoder.com>
2019-07-29 18:06:06 +0530

    changing kafka topic name

--- src/constants.js
+++ src/constants.js
@@ -89,83 +89,83 @@ export const EVENT = {
 };
 
 export const BUS_API_EVENT = {
-  PROJECT_CREATED: 'notifications.connect.project.created',
-  PROJECT_UPDATED: 'connect.action.project.updated',
-  PROJECT_SUBMITTED_FOR_REVIEW: 'notifications.connect.project.submittedForReview',
-  PROJECT_APPROVED: 'notifications.connect.project.approved',
-  PROJECT_PAUSED: 'notifications.connect.project.paused',
-  PROJECT_COMPLETED: 'notifications.connect.project.completed',
-  PROJECT_CANCELED: 'notifications.connect.project.canceled',
-  PROJECT_ACTIVE: 'notifications.connect.project.active',
+  PROJECT_CREATED: 'connect.notification.project.created',
+  PROJECT_UPDATED: 'connect.notification.project.updated',
+  PROJECT_SUBMITTED_FOR_REVIEW: 'connect.notification.project.submittedForReview',
+  PROJECT_APPROVED: 'connect.notification.project.approved',
+  PROJECT_PAUSED: 'connect.notification.project.paused',
+  PROJECT_COMPLETED: 'connect.notification.project.completed',
+  PROJECT_CANCELED: 'connect.notification.project.canceled',
+  PROJECT_ACTIVE: 'connect.notification.project.active',
 
-  PROJECT_PHASE_TRANSITION_ACTIVE: 'notifications.connect.project.phase.transition.active',
-  PROJECT_PHASE_TRANSITION_COMPLETED: 'notifications.connect.project.phase.transition.completed',
-  PROJECT_PHASE_UPDATE_PAYMENT: 'notifications.connect.project.phase.update.payment',
-  PROJECT_PHASE_UPDATE_PROGRESS: 'notifications.connect.project.phase.update.progress',
-  PROJECT_PHASE_UPDATE_SCOPE: 'notifications.connect.project.phase.update.scope',
+  PROJECT_PHASE_TRANSITION_ACTIVE: 'connect.notification.project.phase.transition.active',
+  PROJECT_PHASE_TRANSITION_COMPLETED: 'connect.notification.project.phase.transition.completed',
+  PROJECT_PHASE_UPDATE_PAYMENT: 'connect.notification.project.phase.update.payment',
+  PROJECT_PHASE_UPDATE_PROGRESS: 'connect.notification.project.phase.update.progress',
+  PROJECT_PHASE_UPDATE_SCOPE: 'connect.notification.project.phase.update.scope',
 
-  PROJECT_WORK_TRANSITION_ACTIVE: 'notifications.connect.project.work.transition.active',
-  PROJECT_WORK_TRANSITION_COMPLETED: 'notifications.connect.project.work.transition.completed',
-  PROJECT_WORK_UPDATE_PAYMENT: 'notifications.connect.project.work.update.payment',
-  PROJECT_WORK_UPDATE_PROGRESS: 'notifications.connect.project.work.update.progress',
-  PROJECT_WORK_UPDATE_SCOPE: 'notifications.connect.project.work.update.scope',
+  PROJECT_WORK_TRANSITION_ACTIVE: 'connect.notification.project.work.transition.active',
+  PROJECT_WORK_TRANSITION_COMPLETED: 'connect.notification.project.work.transition.completed',
+  PROJECT_WORK_UPDATE_PAYMENT: 'connect.notification.project.work.update.payment',
+  PROJECT_WORK_UPDATE_PROGRESS: 'connect.notification.project.work.update.progress',
+  PROJECT_WORK_UPDATE_SCOPE: 'connect.notification.project.work.update.scope',
 
-  MEMBER_JOINED: 'notifications.connect.project.member.joined',
-  MEMBER_LEFT: 'notifications.connect.project.member.left',
-  MEMBER_REMOVED: 'notifications.connect.project.member.removed',
-  MEMBER_ASSIGNED_AS_OWNER: 'notifications.connect.project.member.assignedAsOwner',
-  MEMBER_JOINED_COPILOT: 'notifications.connect.project.member.copilotJoined',
-  MEMBER_JOINED_MANAGER: 'notifications.connect.project.member.managerJoined',
+  MEMBER_JOINED: 'connect.notification.project.member.joined',
+  MEMBER_LEFT: 'connect.notification.project.member.left',
+  MEMBER_REMOVED: 'connect.notification.project.member.removed',
+  MEMBER_ASSIGNED_AS_OWNER: 'connect.notification.project.member.assignedAsOwner',
+  MEMBER_JOINED_COPILOT: 'connect.notification.project.member.copilotJoined',
+  MEMBER_JOINED_MANAGER: 'connect.notification.project.member.managerJoined',
 
-  PROJECT_LINK_CREATED: 'notifications.connect.project.linkCreated',
-  PROJECT_FILE_UPLOADED: 'notifications.connect.project.fileUploaded',
-  PROJECT_SPECIFICATION_MODIFIED: 'connect.action.project.updated.spec',
-  PROJECT_PROGRESS_MODIFIED: 'connect.action.project.updated.progress',
-  PROJECT_FILES_UPDATED: 'connect.action.project.files.updated',
-  PROJECT_TEAM_UPDATED: 'connect.action.project.team.updated',
+  PROJECT_LINK_CREATED: 'connect.notification.project.linkCreated',
+  PROJECT_FILE_UPLOADED: 'connect.notification.project.fileUploaded',
+  PROJECT_SPECIFICATION_MODIFIED: 'connect.notification.project.updated.spec',
+  PROJECT_PROGRESS_MODIFIED: 'connect.notification.project.updated.progress',
+  PROJECT_FILES_UPDATED: 'connect.notification.project.files.updated',
+  PROJECT_TEAM_UPDATED: 'connect.notification.project.team.updated',
 
   // When phase is added/updated/deleted from the project,
   // When product is added/deleted from a phase
   // When product is updated on any field other than specification
-  PROJECT_PLAN_UPDATED: 'connect.action.project.plan.updated',
+  PROJECT_PLAN_UPDATED: 'connect.notification.project.plan.updated',
 
-  PROJECT_PLAN_READY: 'connect.action.project.plan.ready',
+  PROJECT_PLAN_READY: 'connect.notification.project.plan.ready',
 
   // When milestone is added/deleted to/from the phase,
   // When milestone is updated for duration/startDate/endDate/status
-  TIMELINE_ADJUSTED: 'connect.action.timeline.adjusted',
+  TIMELINE_ADJUSTED: 'connect.notification.timeline.adjusted',
 
   // When specification of a product is modified
-  PROJECT_PRODUCT_SPECIFICATION_MODIFIED: 'connect.action.project.product.update.spec',
+  PROJECT_PRODUCT_SPECIFICATION_MODIFIED: 'connect.notification.project.product.update.spec',
 
   // When specification of a work item is modified
-  PROJECT_WORKITEM_SPECIFICATION_MODIFIED: 'connect.action.project.workitem.update.spec',
+  PROJECT_WORKITEM_SPECIFICATION_MODIFIED: 'connect.notification.project.workitem.update.spec',
 
-  MILESTONE_ADDED: 'connect.action.timeline.milestone.added',
-  MILESTONE_REMOVED: 'connect.action.timeline.milestone.removed',
-  MILESTONE_UPDATED: 'connect.action.timeline.milestone.updated',
+  MILESTONE_ADDED: 'connect.notification.timeline.milestone.added',
+  MILESTONE_REMOVED: 'connect.notification.timeline.milestone.removed',
+  MILESTONE_UPDATED: 'connect.notification.timeline.milestone.updated',
   // When milestone is marked as active
-  MILESTONE_TRANSITION_ACTIVE: 'connect.action.timeline.milestone.transition.active',
+  MILESTONE_TRANSITION_ACTIVE: 'connect.notification.timeline.milestone.transition.active',
   // When milestone is marked as completed
-  MILESTONE_TRANSITION_COMPLETED: 'connect.action.timeline.milestone.transition.completed',
+  MILESTONE_TRANSITION_COMPLETED: 'connect.notification.timeline.milestone.transition.completed',
    // When milestone is marked as paused
-  MILESTONE_TRANSITION_PAUSED: 'connect.action.timeline.milestone.transition.paused',
+  MILESTONE_TRANSITION_PAUSED: 'connect.notification.timeline.milestone.transition.paused',
   // When milestone is waiting for customers's input
-  MILESTONE_WAITING_CUSTOMER: 'connect.action.timeline.milestone.waiting.customer',
+  MILESTONE_WAITING_CUSTOMER: 'connect.notification.timeline.milestone.waiting.customer',
 
   // TC Message Service events
-  TOPIC_CREATED: 'notifications.connect.project.topic.created',
-  TOPIC_UPDATED: 'notifications.connect.project.topic.updated',
-  POST_CREATED: 'notifications.connect.project.post.created',
-  POST_UPDATED: 'notifications.connect.project.post.edited',
+  TOPIC_CREATED: 'connect.notification.project.topic.created',
+  TOPIC_UPDATED: 'connect.notification.project.topic.updated',
+  POST_CREATED: 'connect.notification.project.post.created',
+  POST_UPDATED: 'connect.notification.project.post.edited',
 
   // Project Member Invites
-  PROJECT_MEMBER_INVITE_CREATED: 'notifications.connect.project.member.invite.created',
-  PROJECT_MEMBER_INVITE_REQUESTED: 'notifications.connect.project.member.invite.requested',
-  PROJECT_MEMBER_INVITE_UPDATED: 'notifications.connect.project.member.invite.updated',
-  PROJECT_MEMBER_INVITE_APPROVED: 'notifications.connect.project.member.invite.approved',
-  PROJECT_MEMBER_INVITE_REJECTED: 'notifications.connect.project.member.invite.rejected',
-  PROJECT_MEMBER_EMAIL_INVITE_CREATED: 'connect.action.email.project.member.invite.created',
+  PROJECT_MEMBER_INVITE_CREATED: 'connect.notification.project.member.invite.created',
+  PROJECT_MEMBER_INVITE_REQUESTED: 'connect.notification.project.member.invite.requested',
+  PROJECT_MEMBER_INVITE_UPDATED: 'connect.notification.project.member.invite.updated',
+  PROJECT_MEMBER_INVITE_APPROVED: 'connect.notification.project.member.invite.approved',
+  PROJECT_MEMBER_INVITE_REJECTED: 'connect.notification.project.member.invite.rejected',
+  PROJECT_MEMBER_EMAIL_INVITE_CREATED: 'connect.notification.email.project.member.invite.created',
 };
 
 export const REGEX = {
