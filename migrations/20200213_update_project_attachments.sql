9c32cedc5313416e90fe5cb10d3e200fe4070c92
Maksym Mykhailenko <maxceem@gmail.com>
2020-02-28 14:50:51 +0800

    fix: migration script for attachments

--- migrations/20200213_update_project_attachments.sql
+++ migrations/20200213_update_project_attachments.sql
@@ -3,10 +3,14 @@
 -- update column : rename file_path to path
 
 -- Add 'type' column, type is the attachment type : 'file' or 'link'
-ALTER TABLE project_attachments ADD COLUMN "type" character varying(255) NOT NULL;
+ALTER TABLE project_attachments ADD COLUMN "type" character varying(255);
+-- As all existent data is files, set the "type" to file
+UPDATE project_attachments SET "type"='file';
+-- Set not null
+ALTER TABLE project_attachments ALTER COLUMN "type" SET NOT NULL;
 
 -- Add 'tags' column, the attachment tags
-ALTER TABLE project_attachments ADD COLUMN tags _varchar NULL;
+ALTER TABLE project_attachments ADD COLUMN tags character varying(255)[] NULL;
 
 -- Rename 'file_path' column to 'path'
 ALTER TABLE project_attachments RENAME COLUMN "filePath" TO "path";
