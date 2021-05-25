9767428e8d2083d7c73dfd968c3897169053cd78
Vikas Agarwal <vikas.agarwal@topcoder.com>
2021-05-25 19:32:18 +0530

    git#651-Return BA status in the GET billing account endpoint — Allowing non m2m users to call the GET billing account details endpoint with sanitized fields

--- src/routes/billingAccounts/get.js
+++ src/routes/billingAccounts/get.js
@@ -45,6 +45,12 @@ module.exports = [
       const sql = `SELECT  TopCoder_Billing_Account_Id__c, Mark_Up__c, Active__c from Topcoder_Billing_Account__c tba where TopCoder_Billing_Account_Id__c='${billingAccountId}'`;
       req.log.debug(sql);
       const billingAccount = await SalesforceService.queryBillingAccount(sql, accessToken, instanceUrl, req.log);
+      const isMachineToken = _.get(req, 'authUser.isMachine', false);
+      if (!isMachineToken) {
+        // delete sensitive information for non machine access
+        // does not revalidate the scope as it assumes that is already taken care
+        delete billingAccount.markup;
+      }
       res.json(billingAccount);
     } catch (error) {
       req.log.error(error);
