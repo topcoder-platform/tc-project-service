74cd2794d685a3f9cd2fc880d7a8de4b2d777822
Justin Gasper <jmgasper@gmail.com>
2023-11-15 20:34:32 +1100

    Handle new jobs on an update

--- src/events/projects/postTaasJobs.js
+++ src/events/projects/postTaasJobs.js
@@ -2,7 +2,7 @@
  * Event handler for the creation of `talent-as-a-service` projects.
  */
 
-import _ from 'lodash';
+import _, { forEach } from 'lodash';
 import config from 'config';
 import axios from 'axios';
 import models from '../../models';
@@ -75,9 +75,6 @@ async function createTaasJobsFromProject(req, project, logger) {
   await projectWithJobs.save();
 }
 
-module.exports = createTaasJobsFromProject;
-
-
 /**
  * Update taas job.
  *
@@ -114,15 +111,29 @@ async function updateTaasJob(authHeader, data) {
  * @return {Object} the taas jobs created
  */
 async function updateTaasJobsFromProject(req, project, logger) {
-  const jobs = _.get(project, 'details.taasDefinition.taasJobs');
-  if (!jobs || !jobs.length) {
+  const originalJobs = _.get(project, 'details.taasDefinition.taasJobs');
+  if (!originalJobs || !originalJobs.length) {
     logger.debug(`No jobs found in the project id to update: ${project.id}`);
     return;
   }
-  logger.debug(`${jobs.length} jobs found in the project id to update: ${project.id}`);
+  logger.debug(`${originalJobs.length} jobs found in the project id to update: ${project.id}`);
+
+  const updateJobs = [];
+  const createJobs = [];
+
+  // Split new jobs out from ones that need to be updated
+  // If a job already has an ID assigned, assume it needs to be updated
+  forEach(originalJobs, (job) => {
+    if (job.jobId) {
+      updateJobs.push(job);
+    } else {
+      createJobs.push(job);
+    }
+  });
+
   await Promise.all(
     _.map(
-      jobs,
+      updateJobs,
       job => updateTaasJob(req.headers.authorization, {
         jobId: job.jobId,
         title: job.title,
@@ -132,13 +143,44 @@ async function updateTaasJobsFromProject(req, project, logger) {
         numPositions: Number(job.people),
         resourceType: _.get(job, 'role.value', ''),
         workload: _.get(job, 'workLoad.title', '').toLowerCase(),
-      }).then((createdJob) => {
-        logger.debug(`jobId: ${createdJob.id} job updated with title "${createdJob.title}"`);
+      }).then((updatedJob) => {
+        logger.debug(`jobId: ${updatedJob.id} job updated with title "${updatedJob.title}"`);
       }).catch((err) => {
         logger.error(`Unable to update job with title "${job.title}": ${err.message}`);
       }),
     ),
   );
+
+  await Promise.all(
+    _.map(
+      createJobs,
+      job => createTaasJob(req.headers.authorization, {
+        projectId: project.id,
+        title: job.title,
+        description: job.description,
+        duration: Number(job.duration),
+        skills: job.skills,
+        numPositions: Number(job.people),
+        resourceType: _.get(job, 'role.value', ''),
+        rateType: 'weekly', // hardcode for now
+        workload: _.get(job, 'workLoad.title', '').toLowerCase(),
+      }).then((createdJob) => {
+        logger.debug(`jobId: ${createdJob.id} job created with title "${createdJob.title}"`);
+        /* eslint no-param-reassign: "error" */
+        job.jobId = createdJob.id;
+      }).catch((err) => {
+        logger.error(`Unable to create job with title "${job.title}": ${err.message}`);
+      }),
+    ),
+  );
+  const jobs = _.concat(updateJobs, createJobs);
+  const projectWithJobs = await models.Project.findByPk(project.id);
+  if (!projectWithJobs) {
+    logger.error(`Project not found for id ${project.id}, so couldn't save TaaS Job IDs`);
+  }
+  projectWithJobs.details.taasDefinition.taasJobs = jobs;
+  projectWithJobs.changed('details', true);
+  await projectWithJobs.save();
 }
 
 module.exports = {
