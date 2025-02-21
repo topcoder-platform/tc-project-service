6ba06716e254a4e2846dc1f3ba9f3e01fa9713c2
himaniraghav3 <himaniraghav@gmail.com>
2025-02-21 10:52:21 +0530

    PM-834 Get copilot opportunity endpoint

---
+++ src/routes/copilotOpportunity/get.js
@@ -0,0 +1,36 @@
+import _ from 'lodash';
+
+import models from '../../models';
+import { ADMIN_ROLES } from '../../constants';
+import util from '../../util';
+import { PERMISSION } from '../../permissions/constants';
+
+module.exports = [
+  (req, res, next) => {
+    const { id } = req.params;
+
+    if (!id || isNaN(id)) {
+      return util.handleError('Invalid opportunity ID', null, req, next, 400);
+    }
+
+    models.CopilotOpportunity.findOne({ 
+        where: { id },
+        include: [
+            {
+                model: models.CopilotRequest, 
+                as: 'copilotRequest',
+            },
+            {
+                model: models.Project,
+                as: 'project',
+            }
+        ],
+    })
+      .then((copilotOpportunity) => {
+        res.json(copilotOpportunity);
+      })
+      .catch((err) => {
+        util.handleError('Error fetching copilot opportunity', err, req, next);
+      });
+  },
+];
