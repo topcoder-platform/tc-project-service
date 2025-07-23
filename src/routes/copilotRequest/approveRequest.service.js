f79f42bfef7efa946a943e729ba4072d553cff0e
Vasilica Olariu <olariu.vasilica@gmail.com>
2025-07-23 10:48:13 +0300

    fix recipients

--- src/routes/copilotRequest/approveRequest.service.js
+++ src/routes/copilotRequest/approveRequest.service.js
@@ -68,7 +68,7 @@ module.exports = (req, data, existingTransaction) => {
                 const copilotPortalUrl = config.get('copilotPortalUrl');
                 req.log.info("Sending emails to all copilots about new opportunity");
 
-                const sendNotification = (userName, recipients) => createEvent(emailEventType, {
+                const sendNotification = (userName, recipient) => createEvent(emailEventType, {
                   data: {
                     user_name: userName,
                     opportunity_details_url: `${copilotPortalUrl}/opportunity/${opportunity.id}`,
@@ -78,14 +78,14 @@ module.exports = (req, data, existingTransaction) => {
                     start_date: moment.utc(startDate).format("YYYY-MM-DD HH:mm:ss [UTC]"),
                   },
                   sendgrid_template_id: TEMPLATE_IDS.CREATE_REQUEST,
-                  recipients,
+                  recipients: [recipient],
                   version: 'v3',
                 }, req.log);
 
                 subjects.forEach(subject => sendNotification(subject.handle, subject.email));
 
                 // send email to notify via slack
-                sendNotification('Copilots', [config.copilotsSlackEmail]);
+                sendNotification('Copilots', config.copilotsSlackEmail);
 
                 req.log.info("Finished sending emails to copilots");
                 
