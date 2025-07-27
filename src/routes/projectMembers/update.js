af842b09b81196575f0838fd0113dec44d463964
Hentry Martin <hentrymartin.p@gmail.com>
2025-07-28 01:55:37 +0200

    fix: complete the copilot requests if the incoming role is observer or customer

--- src/routes/projectMembers/update.js
+++ src/routes/projectMembers/update.js
@@ -119,6 +119,7 @@ module.exports = [
     let updatedProps = req.body;
     const projectId = _.parseInt(req.params.projectId);
     const memberRecordId = _.parseInt(req.params.id);
+    const action = updatedProps.action;
     updatedProps = _.pick(updatedProps, ['isPrimary', 'role']);
     const fields = req.query.fields ? req.query.fields.split(',') : null;
 
@@ -153,7 +154,7 @@ module.exports = [
         req.log.debug(`updated props ${JSON.stringify(updatedProps)}`);
         req.log.debug(`previous values ${JSON.stringify(previousValue)}`);
         // no updates if no change
-        if ((updatedProps.role === previousValue.role || updatedProps.action === 'overwrite') &&
+        if ((updatedProps.role === previousValue.role || action === 'overwrite') &&
               (_.isUndefined(updatedProps.isPrimary) ||
                 updatedProps.isPrimary === previousValue.isPrimary)) {
           await completeAllCopilotRequests(req, projectId, _transaction);
